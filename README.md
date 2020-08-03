3dff64ac960660e465dffaa44b319cd1785813d9
Ben Hilliard <benhilliard23@hotmail.com>
2020-08-03 22:35:16 +0930

    Commit with working application and README

---
+++ README.md
@@ -0,0 +1,46 @@
+# Two Table Song Database [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+     
+## Description
+
+A Command Line Application which displays a list of songs and albums from a chosen artist. Data is sourced from a database utilising data from 2 CSV files.
+
+## Table of Contents
+
+* [Installation](##installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions?](#questions)
+
+
+## Installation
+
+NPM Install is required to ensure that MySQL and prompts can be used. Applications runs in node.js.
+
+## Usage
+
+THe application works by the user answering a series of prompts with questions about their application. Once the questions are answered, a README is generated that they can use in their application.  
+
+Screenshot located at:
+
+Assets\Screenshot\Screenshot.png
+
+## License
+
+This application is covered under GNU GPL v3.0
+    
+## Contributing
+
+Please use the contact details below
+
+## Tests
+
+The application has been tested however no formal tests have been written
+    
+## Questions?
+
+Please contact us at:
+
+GitHub: github.com/BH-2312, https://github.com/EMoschos, https://github.com/Andrew836-dev, https://github.com/CR-53, https://github.com/wongcmk
+
---
+++ albums.js
@@ -0,0 +1,43 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var myQuery = 'SELECT Top5000.year, TopAlbums.position, Top5000.artist, Top5000.song, TopAlbums.album_name FROM Top5000 inner JOIN TopAlbums ON Top5000.artist = TopAlbums.artist and Top5000.year = TopAlbums.year and Top5000.artist = ? ORDER BY Top5000.ARTIST, Top5000.year';
+
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Hotdog88",
+    database: "top_songsDB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    albumSearch();
+});
+
+function albumSearch() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Which Artist Would you like to search?",
+            name: "artistSearch",
+        }
+    ]).then(function (artist) {
+        var query = connection.query(myQuery, artist.artistSearch, function (err, res) {
+            if (err) throw err;
+            // for (var i = 0; i < res.length; i++) {
+            //     console.log((i + 1) + ".)" + "Year:" + res[i].year + " | " + res[i].position + " | " + res[i].artist + " |" + res[i].song + " |" + res[i].album_name);
+            res.forEach((song,index) => console.log(`${index + 1}.) Year: ${song.year} || Album Position: ${song.position} || Artist: ${song.artist} || Song: ${song.song} || Album: ${song.album_name}`));
+        })
+        connection.end();
+    })
+};
